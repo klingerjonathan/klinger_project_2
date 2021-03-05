60aa6060e75d035abb374d2fcf228d9a424ecec7
Jonathan <klinger603@gmail.com>
2021-03-05 14:54:39 +0100

    erste GET tests mit cpp-httplib mithilfe von ZIWAX API

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,33 @@
+/* 
+ * auth: Klinger Jonathan 
+ * file: main.cpp
+ * date: 04.03.21
+ */
 
 #include <iostream>
 #include <thread>
+
+
+//#define CPPHTTPLIB_OPENSSL_SUPPORT
+#include "httplib.h"
+
 
 using namespace std;
 
 //int main(int argc, char* argv[]) {
 int main() {
-    thread t{[]{ cout << "Hello"; }};
-    t.join();
-    cout << " world!" << endl;
+    httplib::Client cli("http://ziwax.de:3002");
+
+
+    //Teste GET mit Ziwax API
+    if (auto res = cli.Get("/firmen")) {
+        if (res->status == 200) {
+            std::cout << res->body << std::endl;
+        }   
+    } else {
+      //auto err = res.error();
+      std::cout << "Oh nein! " << res.error() << std::endl;
+    }
+
     return 0;
 }
